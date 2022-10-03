7f8831a9f3adef42487a5bd57b7fc9de607b8439
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-03 22:09:57 +0200

    working on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,23 @@
+import { Box, Button, Typography } from '@mui/material'
+import { Container } from '@mui/system'
 import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <Box sx={{ minHeight: `calc(100vh - 5rem)`, position: "relative" }}>
+      <Container maxWidth="lg" sx={{
+        position: "absolute", top: "50%",
+        left: "50%", transform: `translate(${-50}%, ${-50}%)`,
+        textAlign:"center"
+      }}>
+        <Typography variant='h1' sx={{mb:"5rem"}}>Ewelina & Lukasz</Typography>
+        <Typography sx={{maxWidth:"35em", margin:"0 auto"}}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum quidem
+          possimus magni labore accusamus hic inventore provident, beatae iure
+          ducimus?
+        </Typography>
+        <Button sx={{mt:"5rem",fontSize:"1.5rem"}} variant="contained" >Confirm attendance</Button>
+      </Container>
+    </Box>
   )
 }
