f68a8cdff4d68e665834ab277f6bb054089f3a75
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-07 11:37:11 +0200

    work with svg

--- src/components/LocationParty/LocationParty.js
+++ src/components/LocationParty/LocationParty.js
@@ -2,6 +2,9 @@ import { Grid, Typography } from "@mui/material";
 import { Box, Container, Stack } from "@mui/system";
 import React from "react";
 import GoogleMapReact from 'google-map-react';
+import church from "../../../public/church.svg";
+import wine from "../../../public/wine.svg";
+import Image from "next/image";
 
 //https://www.youtube.com/watch?v=OGTG1l7yin4
 //https://www.npmjs.com/package/google-map-react
@@ -45,21 +48,50 @@ export const LocationParty = () => {
                 spacing={2}
                 sx={{
                   mb: 5,
-                  textAlign: { xs: "center", md: "left" },
+                  textAlign:'left'
                 }}
               >
-                <Typography variant="h3">Join us for the wedding </Typography>
-                <Typography sx={{ color: "text.secondary" }}>
-                  The Wedding will take place in
-                  Saint Pro-Cathedral
-                  83 Marlborough Street, Oslo
-                  at 10.00 a.m
-                </Typography>
-                <Typography sx={{ color: "text.secondary" }}>
+                <Typography variant="h3" sx={{mb:"2.5rem"}}>Join us for the wedding </Typography>
+                <Box sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  mb:"2rem"
+                }}>
+                  <Box
+                    sx={{
+                      width: "100px",
+                    }}
+                  >
+                    <Image src={church} alt="image" />
+                  </Box>
+                  <Typography variant="body2" sx={{ maxWidth: '18rem', ml:"2rem" }}>
+                    The Wedding will take place in
+                    Saint Pro-Cathedral
+                    83 Marlborough Street, Oslo
+                    at 10.00 a.m
+                  </Typography>
+                </Box>
+
+                <Box sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}>
+                  <Box
+                    sx={{
+
+                      width: "100px",
+
+                    }}
+                  >
+                    <Image src={wine} alt="image" />
+                  </Box>
+                  <Typography variant="body2" sx={{  maxWidth: '18rem',ml:"2rem"  }}>
                   Dinner and dancing will take place in
                   Castleknock Hotel & Country Club
-                  Castleknock, Oslo 
-                </Typography>
+                  Castleknock, Oslo
+                  </Typography>
+                </Box>
+
               </Stack>
             </Grid>
 
