3d4705239a58182a7210cf962bb44bcd5e3f58a4
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-09 18:45:26 +0200

    work on map section , added img

--- src/components/LocationChurch/LocationChurch.js
+++ src/components/LocationChurch/LocationChurch.js
@@ -3,19 +3,19 @@ import { Box, Container, Stack } from "@mui/system";
 import React from "react";
 
 import church from "../../../public/church.svg";
-
+import map from "../../../public/map.png";
 import Image from "next/image";
 
 //https://www.npmjs.com/package/google-map-react
 //https://leafletjs.com/index.html
 
-const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15993.067311743673!2d10.67386275!3d59.92992835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416d95a2fd9315%3A0xb5d6eb5de3a26594!2sNorwegian%20Radium%20Hospital!5e0!3m2!1spl!2sno!4v1665298540040!5m2!1spl!2sno" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15993.067311743673!2d10.67386275!3d59.92992835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416d95a2fd9315%3A0xb5d6eb5de3a26594!2sNorwegian%20Radium%20Hospital!5e0!3m2!1spl!2sno!4v1665298540040!5m2!1spl!2sno" width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 
 export const LocationChurch = () => {
   return (
     <Box
       sx={{
-        minHeight: "100vh",
+        pb: "5rem",
         pt: "5rem",
       }}
     >
@@ -29,13 +29,14 @@ export const LocationChurch = () => {
 
         <Grid container justifyContent="space-between">
           <Grid item xs={12} md={6} lg={6}>
-            <Typography variant="h3" sx={{ mb: "2.5rem" }}>
+            <Typography variant="h3" sx={{ mb: "2.5rem", textAlign: { xs: "center", md: "left" } }}>
               Join us for the wedding{" "}
             </Typography>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "column",
+                alignItems: { xs: "center", md: "flex-start" },
                 mb: "2rem",
               }}
             >
@@ -63,10 +64,19 @@ export const LocationChurch = () => {
           </Grid>
 
           <Grid item xs={12} md={6} lg={6}>
-            <div
-              style={{ width: "400px" }}
-              dangerouslySetInnerHTML={{ __html: iframe }}
-            />
+            <a
+              href="https://goo.gl/maps/3q59iGCWT1WseYMK7"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Box
+                sx={{
+
+                }}
+              >
+                <Image src={map} alt="image" />
+              </Box>
+            </a>
           </Grid>
         </Grid>
       </Container>
--- src/components/LocationParty/LocationParty.js
+++ src/components/LocationParty/LocationParty.js
@@ -2,37 +2,36 @@ import { Grid, Typography } from "@mui/material";
 import { Box, Container, Stack } from "@mui/system";
 import React from "react";
 
-import wine from "../../../public/wine.svg";
 import Image from "next/image";
+import wine from "../../../public/wine.svg";
+
+import map from "../../../public/map.png";
 
 //https://www.npmjs.com/package/google-map-react
 //https://leafletjs.com/index.html
 
-const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15993.067311743673!2d10.67386275!3d59.92992835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416d95a2fd9315%3A0xb5d6eb5de3a26594!2sNorwegian%20Radium%20Hospital!5e0!3m2!1spl!2sno!4v1665298540040!5m2!1spl!2sno" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15993.067311743673!2d10.67386275!3d59.92992835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416d95a2fd9315%3A0xb5d6eb5de3a26594!2sNorwegian%20Radium%20Hospital!5e0!3m2!1spl!2sno!4v1665298540040!5m2!1spl!2sno" width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 
 export const LocationParty = () => {
   return (
     <Box
       sx={{
-        minHeight: "100vh",
-        pb: "5rem",
+        pt: "5rem",
+        pb: "15rem",
       }}
     >
       <Container maxWidth="lg">
         <Grid container justifyContent="space-between">
+
           <Grid item xs={12} md={6} lg={6}>
-            <div dangerouslySetInnerHTML={{ __html: iframe }} />
-          </Grid>
-          <Grid item xs={12} md={6} lg={6}>
-            <Typography variant="h3" sx={{ mb: "2.5rem", textAlign: "right" }}>
-              Join us for the wedding{" "}
+            <Typography variant="h3" sx={{ mb: "2.5rem", textAlign: { xs: "center", md: "left" } }}>
+              Join us for the party
             </Typography>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                alignItems: "flex-end",
-                justifyContent: "flex-end",
+                alignItems: { xs: "center", md: "flex-start" },
                 mb: "2rem",
               }}
             >
@@ -45,18 +44,33 @@ export const LocationParty = () => {
               </Box>
               <Typography
                 variant="body2"
-                sx={{ maxWidth: "20rem", textAlign: "right", mt: "1.5rem" }}
+                sx={{ maxWidth: "20rem", textAlign: "left", mt: "1.5rem" }}
               >
                 Dinner and dancing will take place in Castleknock Hotel &
                 Country Club Castleknock, Oslo
               </Typography>
               <Typography
                 variant="body2"
-                sx={{ maxWidth: "20rem", textAlign: "right", mt: "1.5rem" }}
+                sx={{ maxWidth: "20rem", textAlign: "left", mt: "1.5rem" }}
               >
                 Free parking is available
               </Typography>
             </Box>
+          </Grid>
+          <Grid item xs={12} md={6} lg={6}>
+            <a
+              href="https://goo.gl/maps/3q59iGCWT1WseYMK7"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Box
+                sx={{
+
+                }}
+              >
+                <Image src={map} alt="image" />
+              </Box>
+            </a>
           </Grid>
         </Grid>
       </Container>
