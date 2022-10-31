8a542e56d59e06e93a39bb1ac3151845d28fca23
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-31 19:53:41 +0100

    working on PIECHART fo displaying summary drinks in the chart, added loading when we wait for data

--- pages/summary.js
+++ pages/summary.js
@@ -1,42 +1,37 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Card, Container, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import clientPromise from "../lib/mongodb";
 import CardDataSummary from "../src/components/CardDataSummary/CardDataSummary";
+import { PieChart } from "../src/components/PieChart/PieChart";
 
 const amountPeople = 100;
 
 export default function Summary({ isConnected }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [userDataDrinks, setUserDataDrinks] = useState([]);
+
 
   useEffect(() => {
-    //start loading when data is fetched
     setLoading(true);
 
     async function getData() {
       const response = await fetch("/api/get-data");
       const responseData = await response.json();
-      console.log("clickHandler", { responseData });
-
       const all = responseData.all;
-      console.log("all", { all });
       setData(all);
-
-      //stop loading when data is fetched
       setLoading(false);
     }
 
     getData();
   }, []);
 
-  console.log(data);
+  console.log("data featch", data);
 
   // How many people is coming
   const confirmedPeople = data.filter((person) => {
     return person.isComing === "Yes";
   });
-
-  console.log("confirmedPeople", confirmedPeople.length);
 
   // How many people is not coming
   const amountNotComingPeople = data.filter((person) => {
@@ -49,85 +44,197 @@ export default function Summary({ isConnected }) {
     Number(confirmedPeople.length) -
     Number(amountNotComingPeople.length);
 
-  const nutsAllergy = data.filter((person) => {
+  //drinks
+  const vodkaAmount = data.filter((person) => {
+    return person.isVodka === true;
+  });
+  const ginAmount = data.filter((person) => {
+    return person.isGin === true;
+  });
+  const whiskyAmount = data.filter((person) => {
+    return person.isWhisky === true;
+  });
+  const beerAmount = data.filter((person) => {
+    return person.isBeer === true;
+  });
+  const isNonAlcoholAmount = data.filter((person) => {
+    return person.isNonAlcohol === true;
+  });
+
+
+
+  useEffect(() => {
+    setUserDataDrinks([
+      {
+        id: 1,
+        drink: "Vodka cocktails",
+        userGain: vodkaAmount.length,
+      },
+      {
+        id: 2,
+        drink: "Gin cocktails",
+        userGain: ginAmount.length,
+      },
+      {
+        id: 3,
+        drink: "Whisky cocktails",
+        userGain: whiskyAmount.length,
+      },
+      {
+        id: 4,
+        drink: "Beer",
+        userGain: beerAmount.length,
+      },
+      {
+        id: 5,
+        drink: "non-alcoholic cocktail",
+        userGain: isNonAlcoholAmount.length,
+      },
+    ]);
+  }, [
+    vodkaAmount.length,
+    ginAmount.length,
+    whiskyAmount.length,
+    beerAmount.length,
+    isNonAlcoholAmount.length,
+  ]);
+
+  const userData = {
+    labels: userDataDrinks?.map((data) => data?.drink),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: userDataDrinks?.map((data) => data?.userGain),
+        backgroundColor: [
+          "rgba(155, 199, 232, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(155, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(155, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(155, 199, 232, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(155, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(155, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  console.log("userDataDrink", userDataDrinks);
+
+  const PeanutsPeopleAllergies = data.filter((person) => {
+    return person.isPeanuts === true;
+  });
+  const EggsPeopleAllergies = data.filter((person) => {
+    return person.isEggs === true;
+  });
+  const NutsPeopleAllergies = data.filter((person) => {
     return person.isNuts === true;
   });
 
-  console.log("nutsAllergy", nutsAllergy);
   return (
     <div>
-      {/* {data.map((person) => {
-                return (
-                    <div key={person.id}>
-                        <p>{person.firstName}</p>
-                    </div>
-                )
+      {loading ? (
+        <Typography
+          variant="h3"
+          sx={{
+            mb: 5,
+            textAlign: "center",
+          }}
+        >
+          Loading...
+        </Typography>
+      ) : (
+        <Box
+          sx={{
+            // height: "100vh",
+            pt: "5rem",
+            pb: "15rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Container maxWidth="xl">
+            <Typography
+              variant="h3"
+              sx={{
+                mb: 5,
+                textAlign: "center",
+              }}
+            >
+              Summary of your guest wedding invitations
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6} md={3}>
+                <CardDataSummary
+                  title="Confirmed"
+                  total={confirmedPeople.length}
+                  icon={"akar-icons:people-group"}
+                  colorIcon="#20A4F3"
+                />
+              </Grid>
 
-            })} */}
+              <Grid item xs={12} sm={6} md={3}>
+                <CardDataSummary
+                  title="Not Coming"
+                  total={amountNotComingPeople.length}
+                  color="info"
+                  icon={"emojione-monotone:no-pedestrians"}
+                  colorIcon="#011627"
+                />
+              </Grid>
 
-      <Box
-        sx={{
-          // height: "100vh",
-          pt: "5rem",
-          pb: "15rem",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Container maxWidth="xl">
-          <Typography
-            variant="h4"
-            sx={{
-              mb: 5,
-              textAlign: "center",
-              fontSize: { xs: "1.5rem", md: "1.9rem" }
-            }}
-          >
-            Summary of your guest wedding invitations
-          </Typography>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} md={3}>
-              <CardDataSummary
-                title="Confirmed"
-                total={confirmedPeople.length}
-                icon={"akar-icons:people-group"}
-                colorIcon="#20A4F3"
-              />
+              <Grid item xs={12} sm={6} md={3}>
+                <CardDataSummary
+                  title="Children"
+                  total={5}
+                  color="warning"
+                  icon={"ic:round-child-care"}
+                  colorIcon="#2ec4b6"
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6} md={3}>
+                <CardDataSummary
+                  title={"Pending"}
+                  total={amountPendingPeople}
+                  color="error"
+                  icon={"ic:baseline-pending-actions"}
+                  colorIcon="#FF3366"
+                />
+              </Grid>
             </Grid>
-
-            <Grid item xs={12} sm={6} md={3}>
-              <CardDataSummary
-                title="Not Coming"
-                total={amountNotComingPeople.length}
-                color="info"
-                icon={"emojione-monotone:no-pedestrians"}
-                colorIcon="#011627"
-              />
+            <Grid container spacing={3} sx={{ mt: "3rem" }}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    py: 6,
+                    boxShadow: "5px",
+                    textAlign: "center",
+                    backgroundColor: "#FDFDEC",
+                  }}
+                >
+                  <PieChart
+                    chartData={userData}
+                    vodkaAmount={vodkaAmount.length}
+                    ginAmount={ginAmount.length}
+                    whiskyAmount={whiskyAmount.length}
+                    beerAmount={beerAmount.length}
+                    isNonAlcoholAmount={isNonAlcoholAmount.length}
+                  />
+                </Card>
+              </Grid>
             </Grid>
-
-            <Grid item xs={12} sm={6} md={3}>
-              <CardDataSummary
-                title="Children"
-                total={5}
-                color="warning"
-                icon={"ic:round-child-care"}
-                colorIcon="#2ec4b6"
-              />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={3}>
-              <CardDataSummary
-                title={"Pending"}
-                total={amountPendingPeople}
-                color="error"
-                icon={"ic:baseline-pending-actions"}
-                colorIcon="#FF3366"
-              />
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
+          </Container>
+        </Box>
+      )}
     </div>
   );
 }
@@ -135,7 +242,7 @@ export default function Summary({ isConnected }) {
 export async function getServerSideProps() {
   // Connect with MongoDB
   const client = await clientPromise;
-  console.log("[debug] IndexPage. Connected to mongo db");
+
   const isConnected = await client.isConnected();
 
   return {
---
+++ src/components/PieChart/PieChart.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Chart as ChartJS } from "chart.js/auto";
+
+export const PieChart = ({
+  chartData,
+  vodkaAmount,
+  ginAmount,
+  whiskyAmount,
+  beerAmount,
+  isNonAlcoholAmount,
+}) => {
+  console.log("chartData", chartData);
+
+  return (
+    <>
+      <Typography variant="h3" sx={{ mb: "2rem" }}>
+        Summary Drinks
+      </Typography>
+      <Pie data={chartData} />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          mt: "2rem",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="body1" sx={{ opacity: 0.9, textAlign: "left" }}>
+          Vodka cocktails: {vodkaAmount} people
+        </Typography>
+        <Typography variant="body1" sx={{ opacity: 0.9, mb: "0.5rem" }}>
+          {" "}
+          Gin cocktails: {ginAmount} people
+        </Typography>
+        <Typography variant="body1" sx={{ opacity: 0.9, mb: "0.5rem" }}>
+          {" "}
+          Whisky cocktails: {whiskyAmount} people
+        </Typography>
+        <Typography variant="body1" sx={{ opacity: 0.9, mb: "0.5rem" }}>
+          {" "}
+          Beer cocktails: {beerAmount} people
+        </Typography>
+        <Typography variant="body1" sx={{ opacity: 0.9, mb: "0.5rem" }}>
+          {" "}
+          Non-alcoholic cocktails: {isNonAlcoholAmount} people
+        </Typography>
+      </Box>
+    </>
+  );
+};
