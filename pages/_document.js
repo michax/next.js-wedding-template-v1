a60945e09d0976251402fcd79699e6ee2c1b5c2e
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-12-29 15:06:01 +0100

    fixing bug with emotion

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,10 @@
 import * as React from 'react';
 // next
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-// emotion
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import createEmotionServer from '@emotion/server/create-instance';
-// theme
+
 import palette from '../src/theme/palette';
 
-// ----------------------------------------------------------------------
 
-function createEmotionCache() {
-    return createCache({ key: 'css' });
-}
 
 export default class MyDocument extends Document {
     render() {
@@ -52,38 +44,3 @@ export default class MyDocument extends Document {
     }
 }
 
-// ----------------------------------------------------------------------
-
-MyDocument.getInitialProps = async (ctx) => {
-    const originalRenderPage = ctx.renderPage;
-
-    const cache = createEmotionCache();
-    const { extractCriticalToChunks } = createEmotionServer(cache);
-
-    ctx.renderPage = () =>
-        originalRenderPage({
-            enhanceApp: (App) => (props) =>
-            (
-                <CacheProvider value={cache}>
-                    <App {...props} />
-                </CacheProvider>
-            ),
-        });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    const emotionStyles = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags = emotionStyles.styles.map((style) => (
-        <style
-            data-emotion={`${style.key} ${style.ids.join(' ')}`}
-            key={style.key}
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: style.css }}
-        />
-    ));
-
-    return {
-        ...initialProps,
-        styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
-    };
-};
